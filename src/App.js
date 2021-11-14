5078451d4b9605c5eb5c0e3e69de6821b7ca6c10
Oleg Sychev <sychevoa@elfin.tech>
2021-11-14 14:38:41 +0300

    project cleanup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="https://doodleipsum.com/700/outline?i=bf8210036f741844a10667e3d181517a" className="App-logo" alt="logo" />
+
       </header>
     </div>
   );
