bfa213260d3ea7c043766dc0a2d5d95822d30c0a
Oleg Sychev <sychevoa@elfin.tech>
2021-11-16 22:19:05 +0300

    improve mobile view, code cleanup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, {Component} from 'react';
 import {Box, Button, Grid, Paper, Slider} from "@mui/material";
 import Switch from "@mui/material/Switch";
-import * as PropTypes from "prop-types";
-
-function Item(props) {
-    return null;
-}
-
-Item.propTypes = {children: PropTypes.node};
 
 class App extends Component {
     state = {
         isOpen: true,
         distance: 1500,
+        count: 5,
         region: {
             geoLat: 0,
             geoLong: 0
@@ -36,6 +30,7 @@ class App extends Component {
         console.log('Only open ' + this.state.isOpen)
         console.log('Region is ' + this.state.region.geoLat + " " + this.state.region.geoLong)
         console.log('Distance is ' + this.state.distance)
+        console.log('Count is ' + this.state.count)
     }
 
     isOpenHandler = () => {
@@ -50,6 +45,12 @@ class App extends Component {
         })
     }
 
+    cafeCountChangeHandler = (event, newValue) => {
+        this.setState({
+            count: newValue
+        })
+    }
+
     render() {
         const divStyle = {
             textAlign: 'center',
@@ -58,23 +59,16 @@ class App extends Component {
             justifyContent: 'center'
         }
 
-        // const Item = styled(Paper)(({theme}) => ({
-        //     ...theme.typography.body2,
-        //     padding: theme.spacing(1),
-        //     textAlign: 'center',
-        //     color: theme.palette.text.secondary,
-        // }));
-
         return (
             <div style={divStyle}>
                 <div>
                     <img src="https://doodleipsum.com/500x500/outline?i=bf8210036f741844a10667e3d181517a"
-
+                         style={{width: 400, height: 400}}
                          alt="logo"/>
                 </div>
-                <div align="center">
+                <div align="center" style={{marginTop: -50}}>
                     <Box sx={{
-                        height: 150,
+                        height: 250,
                         width: 400,
                         mx: 0.5,
                         display: 'inline-block'
@@ -123,11 +117,34 @@ class App extends Component {
                                     step={250} min={250} max={2500}
                                 /></Paper>
                             </Grid>
+                            <Grid item xs={6}>
+                                <Paper sx={{
+                                    height: 35,
+                                    padding: 1,
+                                    alignItems: 'center',
+                                    alignContent: 'center'
+                                }}>Сколько кафе показывать</Paper>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Paper sx={{
+                                    height: 35,
+                                    padding: 1,
+                                    alignItems: 'center',
+                                    alignContent: 'center'
+                                }}><Slider
+                                    aria-label="count"
+                                    onChange={this.cafeCountChangeHandler}
+                                    defaultValue={5}
+                                    marks
+                                    valueLabelDisplay="on"
+                                    step={1} min={1} max={10}
+                                /></Paper>
+                            </Grid>
                         </Grid>
+                        <Button style={{width: 100, marginTop: 20}}
+                                variant="contained"
+                                onClick={this.showCoordinates}>Найти</Button>
                     </Box>
-                </div>
-                <div style={{marginTop: `20px`}}>
-                    <Button variant="contained" onClick={this.showCoordinates}>Найти</Button>
                 </div>
             </div>
         );
